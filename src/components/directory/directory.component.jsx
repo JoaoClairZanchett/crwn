47bcfce9076c0f582bf4c073ee7e7239985cb670
joao.silveira <joao.silveira@SL-003950>
2020-01-02 13:49:12 -0300

    feat: add collection component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,32 +11,32 @@ class Directory extends React.Component {
             sections: [
                 {
                     title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    imageUrl: 'http://placekitten.com/200/350',
                     id: 1,
                     linkUrl: 'shop/hats'
                 },
                 {
                     title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    imageUrl: 'http://placekitten.com/200/270',
                     id: 2,
                     linkUrl: 'shop/jackets'
                 },
                 {
                     title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    imageUrl: 'http://placekitten.com/250/300',
                     id: 3,
                     linkUrl: 'shop/sneakers'
                 },
                 {
                     title: 'women',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    imageUrl: 'http://placekitten.com/320/400',
                     size: 'large',
                     id: 4,
                     linkUrl: 'shop/womens'
                 },
                 {
                     title: 'men',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    imageUrl: 'http://placekitten.com/200/300',
                     size: 'large',
                     id: 5,
                     linkUrl: 'shop/mens'
---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,249 @@
+const ShopData =  [
+    {
+      id: 1,
+      title: 'Hats',
+      routeName: 'hats',
+      items: [
+        {
+          id: 1,
+          name: 'Brown Brim',
+          imageUrl: 'http://placekitten.com/200/300',
+          price: 25
+        },
+        {
+          id: 2,
+          name: 'Blue Beanie',
+          imageUrl: 'http://placekitten.com/200/300',
+          price: 18
+        },
+        {
+          id: 3,
+          name: 'Brown Cowboy',
+          imageUrl: 'http://placekitten.com/200/300',
+          price: 35
+        },
+        {
+          id: 4,
+          name: 'Grey Brim',
+          imageUrl: 'http://placekitten.com/200/300',
+          price: 25
+        },
+        {
+          id: 5,
+          name: 'Green Beanie',
+          imageUrl: 'http://placekitten.com/200/300',
+          price: 18
+        },
+        {
+          id: 6,
+          name: 'Palm Tree Cap',
+          imageUrl: 'http://placekitten.com/200/300',
+          price: 14
+        },
+        {
+          id: 7,
+          name: 'Red Beanie',
+          imageUrl: 'http://placekitten.com/200/300',
+          price: 18
+        },
+        {
+          id: 8,
+          name: 'Wolf Cap',
+          imageUrl: 'http://placekitten.com/200/300',
+          price: 14
+        },
+        {
+          id: 9,
+          name: 'Blue Snapback',
+          imageUrl: 'http://placekitten.com/200/300',
+          price: 16
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: 'Sneakers',
+      routeName: 'sneakers',
+      items: [
+        {
+          id: 1,
+          name: 'Adidas NMD',
+          imageUrl: 'http://placekitten.com/300/400',
+          price: 220
+        },
+        {
+          id: 2,
+          name: 'Adidas Yeezy',
+          imageUrl: 'http://placekitten.com/300/400',
+          price: 280
+        },
+        {
+          id: 3,
+          name: 'Black Converse',
+          imageUrl: 'http://placekitten.com/300/400',
+          price: 110
+        },
+        {
+          id: 4,
+          name: 'Nike White AirForce',
+          imageUrl: 'http://placekitten.com/300/400',
+          price: 160
+        },
+        {
+          id: 5,
+          name: 'Nike Red High Tops',
+          imageUrl: 'http://placekitten.com/300/400',
+          price: 160
+        },
+        {
+          id: 6,
+          name: 'Nike Brown High Tops',
+          imageUrl: 'http://placekitten.com/300/400',
+          price: 160
+        },
+        {
+          id: 7,
+          name: 'Air Jordan Limited',
+          imageUrl: 'http://placekitten.com/300/400',
+          price: 190
+        },
+        {
+          id: 8,
+          name: 'Timberlands',
+          imageUrl: 'http://placekitten.com/300/400',
+          price: 200
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: 'Jackets',
+      routeName: 'jackets',
+      items: [
+        {
+          id: 1,
+          name: 'Black Jean Shearling',
+          imageUrl: 'http://placekitten.com/400/500',
+          price: 125
+        },
+        {
+          id: 2,
+          name: 'Blue Jean Jacket',
+          imageUrl: 'http://placekitten.com/400/500',
+          price: 90
+        },
+        {
+          id: 3,
+          name: 'Grey Jean Jacket',
+          imageUrl: 'http://placekitten.com/400/500',
+          price: 90
+        },
+        {
+          id: 4,
+          name: 'Brown Shearling',
+          imageUrl: 'http://placekitten.com/400/500',
+          price: 165
+        },
+        {
+          id: 5,
+          name: 'Tan Trench',
+          imageUrl: 'http://placekitten.com/400/500',
+          price: 185
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      routeName: 'womens',
+      items: [
+        {
+          id: 1,
+          name: 'Blue Tanktop',
+          imageUrl: 'http://placekitten.com/500/600',
+          price: 25
+        },
+        {
+          id: 2,
+          name: 'Floral Blouse',
+          imageUrl: 'http://placekitten.com/500/600',
+          price: 20
+        },
+        {
+          id: 3,
+          name: 'Floral Dress',
+          imageUrl: 'http://placekitten.com/500/600',
+          price: 80
+        },
+        {
+          id: 4,
+          name: 'Red Dots Dress',
+          imageUrl: 'http://placekitten.com/500/600',
+          price: 80
+        },
+        {
+          id: 5,
+          name: 'Striped Sweater',
+          imageUrl: 'http://placekitten.com/500/600',
+          price: 45
+        },
+        {
+          id: 6,
+          name: 'Yellow Track Suit',
+          imageUrl: 'http://placekitten.com/500/600',
+          price: 135
+        },
+        {
+          id: 7,
+          name: 'White Blouse',
+          imageUrl: 'http://placekitten.com/500/600',
+          price: 20
+        }
+      ]
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      routeName: 'mens',
+      items: [
+        {
+          id: 1,
+          name: 'Camo Down Vest',
+          imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+          price: 325
+        },
+        {
+          id: 2,
+          name: 'Floral T-shirt',
+          imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+          price: 20
+        },
+        {
+          id: 3,
+          name: 'Black & White Longsleeve',
+          imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+          price: 25
+        },
+        {
+          id: 4,
+          name: 'Pink T-shirt',
+          imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+          price: 25
+        },
+        {
+          id: 5,
+          name: 'Jean Long Sleeve',
+          imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
+          price: 40
+        },
+        {
+          id: 6,
+          name: 'Burgundy T-shirt',
+          imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+          price: 25
+        }
+      ]
+    }
+  ] 
+
+  export default ShopData;
